58159790858e0a9e61d0ca293d59fa57faae570f
Al3xus <95219146+Al3xus@users.noreply.github.com>
2024-12-15 12:10:05 +0200

    Update FuncA.cpp
    updates

--- devops2.1/FuncA.cpp
+++ devops2.1/FuncA.cpp
@@ -1,26 +1,12 @@
 #include "FuncA.h"
-#include <cmath> 
+#include <cmath>
 
+double FuncA::solve(double x, int n) {
+    double sum = 0.0;
 
-int FuncA::solve(int n) {
-    double x = 1.0;  
-    double sum = 0.0; 
-    int sign = 1;  
-
-    for (int i = 0; i < n; ++i) {
-        int termIndex = 2 * i + 1; 
-        double term = sign * std::pow(x, termIndex) / factorial(termIndex);
-        sum += term;
-        sign = -sign;  
+    for (int i = 0; i <= n; ++i) {
+        sum += std::pow(x, i);
     }
 
-    return static_cast<int>(sum); 
-}
-
-int FuncA::factorial(int n) {
-    int result = 1;
-    for (int i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
+    return sum;
 }
