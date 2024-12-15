3354c2a1e52504242d487505a1e7b1dc459897a2
Al3xus <95219146+Al3xus@users.noreply.github.com>
2024-12-15 14:19:55 +0000

    x

--- FuncA.h
+++ FuncA.h
@@ -1,10 +1,5 @@
-#include "FuncA.h"
-
-long long FuncA::factorial(int n) {
-    if (n == 0 || n == 1) return 1;
-    long long result = 1;
-    for (int i = 2; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
-}
+class FuncA {
+public:
+    long long factorial(int n);
+    double solve(double x, int n);
+};
