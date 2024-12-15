7a45217a69f996e7542c1f2a1d3ce75a6b3adf8f
Al3xus <95219146+Al3xus@users.noreply.github.com>
2024-12-15 12:11:06 +0200

    Update main.cpp

--- devops2.1/main.cpp
+++ devops2.1/main.cpp
@@ -3,7 +3,23 @@
 
 int main() {
     FuncA object;
-    int n = 10;
-    std::cout << "Result of FuncA: " << object.solve(n) << std::endl;
+
+    double x;
+    int n;
+
+    std::cout << "Enter the value of x (|x| < 1): ";
+    std::cin >> x;
+
+    if (std::abs(x) >= 1.0) {
+        std::cerr << "Error: |x| must be less than 1 for convergence!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Enter the number of terms (n): ";
+    std::cin >> n;
+
+    double result = object.solve(x, n);
+    std::cout << "Result of the series sum: " << result << std::endl;
+
     return 0;
 }
